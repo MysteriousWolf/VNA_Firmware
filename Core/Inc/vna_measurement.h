9447c796c600ba6d6eaacde9169d32c71365501c
Matej Pevec <matej2000@gmail.com>
2024-09-23 21:21:23 +0200

    Fix the prototype mismatch for vna_set_meas_point_count

--- Core/Inc/vna_measurement.h
+++ Core/Inc/vna_measurement.h
@@ -25,7 +25,7 @@ int32_t vna_set_active_meas(uint32_t meas_idx);
 int32_t vna_set_meas_start_frequency(uint64_t freq);
 int32_t vna_set_meas_stop_frequency(uint64_t freq);
 int32_t vna_set_meas_frequency_range(uint64_t start_freq, uint64_t stop_freq);
-int32_t vna_set_meas_point_count(meas_meta_t *meta, uint32_t count);
+int32_t vna_set_meas_point_count(uint32_t count);
 
 // Setting getters
 uint64_t vna_get_meas_start_frequency();
